b0929cddcb9e598480db7098b344e9657a3f6cf8
LeSauceur <dubasque.simon@gmail.com>
2023-10-24 09:35:43 +0200

    ajout fonction moyenne capteur

--- main.ino
+++ main.ino
@@ -5,5 +5,28 @@ void setup()
 //424664
 void loop()
 {
-    // blablah
+    typedef struct Capteur
+{
+    int type;
+    int min;
+    int max;
+    //int timeout; question a poser au prof
+    int actif;
+    int nombre_erreur;
+    float derniere_valeur[10];
+    int tab_moy_index;
+    float moyenne;
+} Capteur;
+
+Capteur temperature;
+
+float moyenne(Capteur *capteuri){
+    float moyenne = 0;
+    for(int i=0; i<10; i++){
+        moyenne = moyenne + capteuri->derniere_valeur[i];
+    }
+    moyenne = moyenne/10;  
+    return moyenne;
+}
+
 }
