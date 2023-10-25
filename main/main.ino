3ea61ddfabbd764ea3b3432b629d2f6a4f8119bc
Shiyukine <benjbres@gmail.com>
2023-10-25 22:49:45 +0200

    testing theoretical inactivite on config mode

--- main/main.ino
+++ main/main.ino
@@ -24,143 +24,128 @@ int month;
 int day;
 SdFat32 *SD;
 RTC_DS1307 *rtc;
+int previous_mode = -1;
+int inactivite = 0;
+
+void get_commande()
+{
+    int i;
+    int MIN;
+    int MAX;
+
+    if (Serial.available() != 0)
+    {
+        inactivite = millis();
+        String command = Serial.readStringUntil('=');
+        int value = Serial.readStringUntil('\n').toInt();
+
+        if (command.indexOf("LUMIN") > 0)
+        {
+            i = 4;
+            MIN = 0;
+            MAX = 1023;
+        }
+
+        if (command.indexOf("TEMP") > 0)
+        {
+            i = 1;
+            MIN = -40;
+            MAX = 85;
+        }
+
+        if (command.indexOf("HYGR") > 0)
+        {
+            i = 2;
+            MIN = -40;
+            MAX = 85;
+        }
+
+        if (command.indexOf("PRESSURE") > 0)
+        {
+            i = 3;
+            MIN = 300;
+            MAX = 1100;
+        }
+
+        if (command == "LUMIN" || command == "TEMP_AIR" || command == "HYGR" || command == "PRESSURE")
+        {
+            if (value == 0 || value == 1)
+            {
+                capteurs[i]->actif = value;
+                Serial.println("succès de l'operation !!");
+            }
+            else
+            {
+                Serial.println("veuillez entrer une valeur entre 0 et 1.\n");
+            }
+            command = "None";
+        }
+
+        if (command.indexOf("MIN") > 0)
+        {
+            if (value > MIN && value < MAX)
+            {
+                capteurs[i]->min = value;
+                Serial.println("succès de l'operation !!");
+            }
+            else
+            {
+                Serial.println("veuillez entrer une valeur entre " + String(MIN) + " et " + String(MAX) + ".\n");
+            }
+            command = "None";
+        }
+
+        if (command.indexOf("MAX") > 0)
+        {
+            if (value > MIN && value < MAX)
+            {
+                capteurs[i]->max = value;
+                Serial.println("succès de l'operation !!");
+            }
+            else
+            {
+                Serial.println("veuillez entrer une valeur entre " + String(MIN) + " et " + String(MAX) + ".\n");
+            }
+            command = "None";
+        }
+
+        /*
+        for (int i = 0; i < 9; i++)
+        {
+          Serial.println("Nouvelles valeurs actives des capteurs : " + String(capteurs[i]->actif));
+          Serial.println("Nouvelles valeurs des seuil MIN : " + String(capteurs[i]->min));
+          Serial.println("Nouvelles valeurs des seuil MAX : " + String(capteurs[i]->max));
+        }*/
+    }
+}
 
 void erreur(int erreur)
 {
     Serial.println("Error " + String(erreur));
 }
 
-File *changement_fichier(int mess_size)
+void change_mode(int nvmode)
 {
-    DateTime *now = &rtc->now();
-    static File actualFile;
-    static bool firstcall = true;
-    if (firstcall)
+    previous_mode = mode;
+    mode = nvmode;
+    Serial.println(nvmode);
+    if (mode == 1)
     {
-        String aa = String(now->year());
-        aa = String(aa[2]) + String(aa[3]);
-        String mm = String(now->month());
-        if (now->month() < 10)
-            mm = "0" + mm;
-        String jj = String(now->day());
-        if (now->day() < 10)
-            jj = "0" + jj;
-        actualFile = SD->open(aa + mm + jj + "_" + String(compteur_revision) + ".LOG", O_RDWR | O_CREAT | O_TRUNC);
-    }
-    firstcall = false;
-    if (!actualFile)
-        erreur(1);
-    bool changeFile = false;
-    if (actualFile.position() + mess_size > file_max_size)
-    {
-        compteur_revision++;
-        changeFile = true;
-        compteur_taille_fichier = 0;
-    }
-    if (year != now->year() && month != now->month() && day != now->day())
-    {
-        compteur_revision = 0;
-        year = now->year();
-        month = now->month();
-        day = now->day();
-        changeFile = true;
-    }
-    if (changeFile)
-    {
-        String aa = String(now->year());
-        aa = String(aa[2]) + String(aa[3]);
-        String mm = String(now->month());
-        if (now->month() < 10)
-            mm = "0" + mm;
-        String jj = String(now->day());
-        if (now->day() < 10)
-            jj = "0" + jj;
-        Serial.println("Copy to " + aa + mm + jj + "_" + String(compteur_revision) + ".LOG");
-        File newFile = SD->open(aa + mm + jj + "_" + String(compteur_revision) + ".LOG", O_RDWR | O_CREAT | O_TRUNC);
-        if (!newFile)
-        {
-            compteur_revision--;
-            erreur(5);
-        }
-        else
-        {
-            actualFile.seek(0);
-            while (actualFile.available())
-            {
-                int lastPos = actualFile.position();
-                newFile.write(actualFile.read());
-                if (lastPos == actualFile.position())
-                {
-                    erreur(2);
-                    break;
-                }
-            }
-            newFile.flush();
-            newFile.close();
-            actualFile.seek(0);
-            Serial.println(F("Changed file"));
-        }
-    }
-    return &actualFile;
-}
-
-void enregistrement()
-{
-    char mess[] = "Capteur 1 = 25, Capteur 2 = 25, Capteur 3 = 25, Capteur 4 = 25, Capteur 5 = 25, Capteur 6 = 25, Capteur 7 = 25, Capteur 8 = 25, Capteur 9 = 25 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-    File *actualFile = changement_fichier(sizeof(mess) / sizeof(mess[0]));
-    /*for (int i = 0; i < sizeof(capteurs) / sizeof(capteurs[0]); i++)
-    {
-        if (actualFile->availableForWrite())
-            actualFile->print("25 ");
-        else
-            erreur(20);
-        actualFile->flush();
-    }*/
-    int pos = actualFile->position();
-    actualFile->println(mess);
-    if (pos == actualFile->position())
-        erreur(21);
-    else
-        actualFile->flush();
-}
-
-void mode_standard()
-{
-    mode = 1;
-    Serial.begin(115200);
-    Serial.print(F("Initializing SD card..."));
-    SD = new SdFat32();
-    rtc = new RTC_DS1307();
-    if (!SD->begin(4))
-    {
-        Serial.println(F("initialization failed!"));
-        erreur(13);
-    }
-    else
-    {
-        Serial.println(F("initialization done."));
-    }
-    if (!rtc->begin())
-    {
-        Serial.println(F("Horloge introuvable"));
-    }
-    else
-    {
-        DateTime *now = &rtc->now();
-        year = now->year();
-        month = now->month();
-        day = now->day();
+        inactivite = millis();
     }
 }
 
 void setup()
 {
-    mode_standard();
+    change_mode(1);
 }
 
 void loop()
 {
-    if (mode == 1)
-        enregistrement();
-    delay(1e3);
+    if (mode == 1 && millis() - inactivite >= 2000)
+        change_mode(0);
+    else
+    {
+        get_commande();
+    }
 }
